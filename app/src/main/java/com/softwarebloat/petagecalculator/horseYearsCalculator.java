fcc85b5abc222ed8b86505b9b474b179d59450b9
Nico <Nico@nicolos-MacBook-Pro.local>
2016-03-09 18:54:06 +0100

    Add horseYearsCalculator subclass

---
+++ app/src/main/java/com/softwarebloat/petagecalculator/horseYearsCalculator.java
@@ -0,0 +1,29 @@
+package com.softwarebloat.petagecalculator;
+
+/**
+ * Created by Nico on 3/9/16.
+ */
+public class horseYearsCalculator extends yearCalculator {
+
+    private int horse_years[] = {2,8,13,17,20};
+
+    @Override
+    public int calculate(int years) {
+
+        if(years == 0){
+            super.setAge(0);
+        }
+
+        if(years > 0 && years <= 5){
+            super.setAge(horse_years[years-1]);
+        }
+
+        else if(years > 5){
+            float adult = 2.5f;
+            super.setAge((int) (((years)-4) * adult + 20));
+        }
+
+        return super.getAge();
+    }
+
+}
