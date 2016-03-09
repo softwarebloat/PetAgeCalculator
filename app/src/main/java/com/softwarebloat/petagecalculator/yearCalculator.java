4537c5138f62724ad4e747dcbd602fe3a6ace26b
Nico <Nico@nicolos-MacBook-Pro.local>
2016-03-09 15:42:21 +0100

    Add yearCalculator class

---
+++ app/src/main/java/com/softwarebloat/petagecalculator/yearCalculator.java
@@ -0,0 +1,25 @@
+package com.softwarebloat.petagecalculator;
+
+/**
+ * Created by Nico on 3/7/16.
+ */
+public class yearCalculator {
+
+    private int age;
+
+    public int calculate(int years){
+        return 0;
+    }
+
+    public yearCalculator() {
+        this.age = 0;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
