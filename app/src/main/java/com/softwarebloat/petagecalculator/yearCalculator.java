2d34f40560ddb8190413f84d3982e4688077b2b4
Nico <Nico@nicolos-MacBook-Pro.local>
2016-03-10 18:45:59 +0100

    Add type variable plus getter/setter

--- app/src/main/java/com/softwarebloat/petagecalculator/yearCalculator.java
+++ app/src/main/java/com/softwarebloat/petagecalculator/yearCalculator.java
@@ -2,5 +2,15 @@ package com.softwarebloat.petagecalculator;
 
 public abstract class yearCalculator
 {
+    protected int type;
+
     public abstract int calculate(int humanAge);
+
+    protected int getType() {
+        return type;
+    }
+
+    protected void setType(int type) {
+        this.type = type;
+    }
 }
