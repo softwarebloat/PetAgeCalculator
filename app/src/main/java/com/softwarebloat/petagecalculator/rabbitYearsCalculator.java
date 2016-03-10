e7b85d7ab3ca83adc3bd8758d29fdf47fc4ee172
sctt <scottistefano91@gmail.com>
2016-03-10 17:22:50 +0100

    Update rabbitYearsCalculator.java
    code clean.

--- app/src/main/java/com/softwarebloat/petagecalculator/rabbitYearsCalculator.java
+++ app/src/main/java/com/softwarebloat/petagecalculator/rabbitYearsCalculator.java
@@ -1,22 +1,17 @@
 package com.softwarebloat.petagecalculator;
 
-public class rabbitYearsCalculator extends yearCalculator {
-
-    private int bunny_years[] = {21,27,33,39,45,51,57,63,69,75,81,87,93,96,105,111,117,123,129,135};
+public class rabbitYearsCalculator extends yearCalculator
+{
+    private int rabbitYears[] = {0,21,27,33,39,45,51,57,63,69,75,81,87,93,96,105,111,117,123,129,135};
 
     @Override
-    public int calculate(int years) {
-
-        if(years == 0)
-            super.setAge(0);
-
-            //else if(years > 20)
-            //Toast.makeText(getApplicationContext(), R.string.bunny_text_error, Toast.LENGTH_SHORT).show();
-
-        else
-            super.setAge(bunny_years[years-1]);
-
-        return super.getAge();
+    public int calculate(int humanAge)
+    {
+        // TODO: This case should be handled by an exception.
+        if (humanAge > 20) {
+            humanAge = 20;
+        }
+        
+        return rabbitYears[humanAge];
     }
-
 }
