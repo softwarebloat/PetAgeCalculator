90e46a74e2ae9b80ad3ae90ebd6f405a1b3a7fb7
Nico <Nico@nicolos-MacBook-Pro.local>
2016-03-10 19:00:29 +0100

    Set type to rabbit within the constructor

--- app/src/main/java/com/softwarebloat/petagecalculator/rabbitYearsCalculator.java
+++ app/src/main/java/com/softwarebloat/petagecalculator/rabbitYearsCalculator.java
@@ -4,6 +4,10 @@ public class rabbitYearsCalculator extends yearCalculator
 {
     private int rabbitYears[] = {0, 21, 27, 33, 39, 45, 51, 57, 63, 69, 75, 81, 87, 93, 96, 105, 111, 117, 123, 129, 135};
 
+    public rabbitYearsCalculator() {
+        setType(2);
+    }
+
     @Override
     public int calculate(int humanAge)
     {
