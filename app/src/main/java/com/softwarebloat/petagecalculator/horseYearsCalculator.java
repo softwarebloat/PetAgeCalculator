3185f7959df37f492e8e7e45c8a6fc0715d57fcb
sctt <scottistefano91@gmail.com>
2016-03-10 17:30:18 +0100

    Update horseYearsCalculator.java
    code clean.

--- app/src/main/java/com/softwarebloat/petagecalculator/horseYearsCalculator.java
+++ app/src/main/java/com/softwarebloat/petagecalculator/horseYearsCalculator.java
@@ -1,26 +1,17 @@
 package com.softwarebloat.petagecalculator;
 
-public class horseYearsCalculator extends yearCalculator {
-
-    private int horse_years[] = {2,8,13,17,20};
+public class horseYearsCalculator extends yearCalculator
+{
+    private int horseYears[] = {0, 2, 8, 13, 17, 20};
 
     @Override
-    public int calculate(int years) {
-
-        if(years == 0){
-            super.setAge(0);
+    public int calculate(int humanAge)
+    {
+        if (humanAge >= 0 && humanAge <= 5) {
+            return horseYears[humanAge];
         }
-
-        if(years > 0 && years <= 5){
-            super.setAge(horse_years[years-1]);
-        }
-
-        else if(years > 5){
-            float adult = 2.5f;
-            super.setAge((int) (((years)-4) * adult + 20));
-        }
-
-        return super.getAge();
+        
+        int petAge = humanAge - 4;
+        return petAge * 2.5 + 20;
     }
-
 }
