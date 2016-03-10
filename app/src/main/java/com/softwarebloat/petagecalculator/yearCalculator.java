8dc4db2dc9dd4e567e21ef3c407b1d10e774bd7e
sctt <scottistefano91@gmail.com>
2016-03-10 17:03:38 +0100

    Update yearCalculator.java
    removed unnecessary age property;
    added abstract modifiers;
    code cleaned.

--- app/src/main/java/com/softwarebloat/petagecalculator/yearCalculator.java
+++ app/src/main/java/com/softwarebloat/petagecalculator/yearCalculator.java
@@ -1,22 +1,7 @@
 package com.softwarebloat.petagecalculator;
 
-public class yearCalculator {
-
-    private int age;
-
-    public int calculate(int years){
-        return 0;
-    }
-
-    public yearCalculator() {
-        this.age = 0;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
+public abstract class yearCalculator {
+    
+    public abstract int calculate(int humanAge);
+    
 }
