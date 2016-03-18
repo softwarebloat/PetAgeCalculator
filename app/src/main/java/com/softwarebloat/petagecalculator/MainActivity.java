ee49a161c50f977bda585e6b3a620098b52faa1b
Nico <Nico@nicolos-MacBook-Pro.local>
2016-03-18 15:25:08 +0100

    Remove unnecessary code

--- app/src/main/java/com/softwarebloat/petagecalculator/MainActivity.java
+++ app/src/main/java/com/softwarebloat/petagecalculator/MainActivity.java
@@ -2,92 +2,51 @@ package com.softwarebloat.petagecalculator;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
-import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-	String years;
 	TextView mTextView, humanAge;
 	SeekBar age;
     int type = 0;
     int seekbarValue;
 
 
-
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState)
+    {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		//final EditText text = (EditText) findViewById(R.id.editText1);
-		//years = text.getText().toString();
+
 		age = (SeekBar) findViewById(R.id.set_age);
 		mTextView = (TextView) findViewById(R.id.textView1);
 		humanAge = (TextView) findViewById(R.id.human_age);
 
-
-		
-		//Button button = (Button) findViewById(R.id.calc_button);
-		
-		/*button.setOnClickListener(new View.OnClickListener() {
-		    public void onClick(View v) {
-		       int seekbarValue = age.getProgress();
-
-		       if(seekbarValue < 1){
-		    	   //text.setError(getText(R.string.years_text_error));
-		    	   Toast.makeText(getApplicationContext(), R.string.years_text_error, Toast.LENGTH_SHORT).show();
-		       }
-               //Controllo temporaneo per bypassare errore calcolo coniglio se anni > 20
-			   if(seekbarValue > 20 && type == 2){
-				   Toast.makeText(getApplicationContext(), R.string.bunny_text_error, Toast.LENGTH_SHORT).show();
-			   }
-
-		       else{
-		    	   calculateYears(v, type, seekbarValue); //perchè serve la view???
-		       }
-
-		    }
-		});*/
-
-		
 		age.setOnSeekBarChangeListener(new OnSeekBarChangeListener(){
-			int progress = 0;
+
 			@Override
-			public void onProgressChanged(SeekBar seekBar, int progressValue, boolean fromUser) {
-				// TODO Auto-generated method stub
-
+			public void onProgressChanged(SeekBar seekBar, int progressValue, boolean fromUser)
+            {
 				humanAge.setText(String.valueOf(progressValue));
-                //int seekbarValue = age.getProgress();
-                //Toast.makeText(getApplicationContext(), "changing seekbar progress", Toast.LENGTH_SHORT).show();
-
 			}
 
 			@Override
 			public void onStartTrackingTouch(SeekBar seekBar) {
 				// TODO Auto-generated method stub
-                //Toast.makeText(getApplicationContext(), "Touching seekbar", Toast.LENGTH_SHORT).show();
 			}
 
 			@Override
-			public void onStopTrackingTouch(SeekBar seekBar) {
-				// TODO Auto-generated method stub
-                //Toast.makeText(getApplicationContext(), "stopping seekbar progress", Toast.LENGTH_SHORT).show();
+			public void onStopTrackingTouch(SeekBar seekBar)
+            {
                 seekbarValue = age.getProgress();
                 calculateYears(type, seekbarValue);
 			}
-			
-		});
 
-		
+		});
 	}
 
     public void RadioClicked(View view)
@@ -104,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
 
 	public void calculateYears(int animal_type, int years)
     {
-        int human_age = 0;
+        int human_age;
         yearsCalculatorFactory calculator = new yearsCalculatorFactory();
         human_age = calculator.createCalculator(animal_type).calculate(years);
 
