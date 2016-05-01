12d2a12dbeb789609907f52bbf1c2b93cc2335eb
Nicolo Lino <nlino@byte-code.com>
2016-05-01 19:04:31 +0200

    Fix cow years calculator bugs

--- app/src/main/java/com/softwarebloat/petagecalculator/cowYearsCalculator.java
+++ app/src/main/java/com/softwarebloat/petagecalculator/cowYearsCalculator.java
@@ -1,11 +1,8 @@
 package com.softwarebloat.petagecalculator;
 
-/**
- * Created by Nico on 5/1/16.
- */
 public class cowYearsCalculator extends yearCalculator
 {
-    private int cowAge[] = {0, 14, 18, 22, 26, 30, 34, 38, 42, 46, 50, 54, 58, 62, 66, 70, 74, 78, 82, 86, 90};
+    int cowAge = 14;
 
     public cowYearsCalculator()
     {
@@ -15,6 +12,14 @@ public class cowYearsCalculator extends yearCalculator
     @Override
     public int calculate(int humanAge)
     {
-        return cowAge[humanAge];
+        if( humanAge == 0 ){
+            return 0;
+        }
+
+        if( humanAge == 1 ) {
+            return cowAge;
+        }
+
+        return cowAge + (4 * (humanAge-1));
     }
 }
